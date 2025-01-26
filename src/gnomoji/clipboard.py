fc120dd4acfd42f96845f4335a08b9c0254243f2
RiteshRavichandran <riteshravichandran.b@outlook.com>
2025-01-26 04:57:01 +0000

    Gdk specific implemntation for clipboard 📋

--- src/gnomoji/clipboard.py
+++ src/gnomoji/clipboard.py
@@ -1,7 +1,13 @@
 # src/gnomoji/clipboard.py
-import pyperclip
+import gi
+
+gi.require_version("Gtk", "3.0")
+gi.require_version("Gdk", "3.0")
+from gi.repository import Gtk, Gdk
 
 
 def copy_to_clipboard(text):
-    """Copy the given text to the clipboard."""
-    pyperclip.copy(text)
+    """Copy the given text to the clipboard using GTK."""
+    clipboard = Gtk.Clipboard.get(Gdk.SELECTION_CLIPBOARD)
+    clipboard.set_text(text, -1)
+    clipboard.store()
